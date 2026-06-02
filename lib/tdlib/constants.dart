89c666e79118da0c7b1a72d127a14e3bc6aa808e
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-02 12:41:53 -0300

    Add files via upload

---
+++ lib/tdlib/constants.dart
@@ -0,0 +1,21 @@
+// Updates
+const String updateChatFoldersConst = 'UpdateChatFolders';
+const String updateAuthorizationStateConst = 'UpdateAuthorizationState';
+const String updateNewChatConst = 'UpdateNewChat';
+const String updateChatPositionConst = 'UpdateChatPosition';
+const String updateChatLastMessageConst = 'UpdateChatLastMessage';
+const String updateChatAddedToListConst = 'UpdateChatAddedToList';
+const String updateSupergroupConst = 'UpdateSupergroup';
+const String updateChatReadInboxConst = 'UpdateChatReadInbox';
+const String updateSupergroupFullInfoConst = 'UpdateSupergroupFullInfo';
+const String updateFileConst = 'UpdateFile';
+const String updateNewMessageConst = 'UpdateNewMessage';
+const String updateUserConst = 'UpdateUser';
+const String updateUserStatusConst = 'UpdateUserStatus';
+const String updateDeleteMessagesConst = 'UpdateDeleteMessages';
+const String updateUnreadMessageCountConst = 'UpdateUnreadMessageCount';
+
+// Stories
+const String updateStoryConst = 'UpdateStory';
+const String updateStoryDeletedConst = 'UpdateStoryDeleted';
+const String updateStoriesConst = 'UpdateActiveStories';
